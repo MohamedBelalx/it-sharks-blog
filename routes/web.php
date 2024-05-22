b1dc3e9477c927fd94aff039ed7860ca70ffd773
Mohamed Belal <hero.armna@gmail.com>
2024-05-22 19:15:34 +0200

    define routes for posts

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,4 +30,12 @@ Route::group(['prefix' => 'category'], function () {
     Route::get('/edit/{id}', [CategoryController::class, 'edit'])->name('category.edit');
     Route::get('/destroy/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
     Route::post('/update/{id}', [CategoryController::class, 'update'])->name('category.update');
+});
+Route::group(['prefix' => 'posts'], function () {
+    Route::get('/create', [PostsController::class, 'create'])->name('posts.create');
+    Route::post('/store', [PostsController::class, 'store'])->name('posts.store');
+    Route::get('/index', [PostsController::class, 'index'])->name('posts.index');
+    Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('posts.edit');
+    Route::get('/destroy/{id}', [PostsController::class, 'destroy'])->name('posts.destroy');
+    Route::post('/update/{id}', [PostsController::class, 'update'])->name('posts.update');
 });
